5b8e411c6ac5ee0c5a0d84813b639bdce1b1f102
Keneth Miler <miler@itp.uni-frankfurt.de>
2026-05-18 13:43:27 +0200

    Fixed a compilation order mistake

--- src/IO/diagnostics/outflow_diagnostics.cpp
+++ src/IO/diagnostics/outflow_diagnostics.cpp
@@ -1,28 +1,28 @@
 /**
  * @file outflow_diagnostics.cpp
  * @author Carlo Musolino (carlo.musolino@aei.mpg.de)
- * @brief 
+ * @brief
  * @date 2026-01-15
- * 
+ *
  * @copyright This file is part of of the General Relativistic Astrophysics
  * Code for Exascale.
  * GRACE is an evolution framework that uses Finite Volume
  * methods to simulate relativistic spacetimes and plasmas
  * Copyright (C) 2023-2026 Carlo Musolino and GRACE Contributors
- *                                    
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
- *   
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *   
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 #include <grace_config.h>
@@ -41,53 +41,53 @@
 
 #include <array>
 #include <vector>
-#include <string> 
+#include <string>
 
 namespace grace {
 
-std::vector<std::string> outflows::flux_names = {"Mdot_unbound_geo", "Mdot_unbound_bern", "Mdot_tot"} ; 
+std::vector<std::string> outflows::flux_names = {"Mdot_unbound_geo", "Mdot_unbound_bern", "Mdot_tot"} ;
 
 #ifdef GRACE_ENABLE_M1
-#if defined(M1_NU_THREESPECIES)
+#if defined(M1_NU_FIVESPECIES)
 std::vector<std::string> m1_outflows::flux_names = {"Lrad_nue", "Lrad_nuebar", "Lrad_nux"};
-#elif defined(M1_NU_FIVESPECIES)
+#elif defined(M1_NU_THREESPECIES)
 std::vector<std::string> m1_outflows::flux_names = {"Lrad_nue", "Lrad_nuebar", "Lrad_numu", "Lrad_numubar", "Lrad_nux"};
 #else
 std::vector<std::string> m1_outflows::flux_names = {"Lrad_nu1"};
 #endif
-#endif 
+#endif
 
-std::array<double,outflows::n_fluxes> 
+std::array<double,outflows::n_fluxes>
 outflows::compute_local_fluxes(
-    Kokkos::View<double**> ivals_d, 
-    Kokkos::View<double**> ivals_aux_d, 
-    spherical_surface_iface const& detector 
-) 
+    Kokkos::View<double**> ivals_d,
+    Kokkos::View<double**> ivals_aux_d,
+    spherical_surface_iface const& detector
+)
 {
-    GRACE_VERBOSE("Computing outflows on sphere {}", detector.name ) ; 
+    GRACE_VERBOSE("Computing outflows on sphere {}", detector.name ) ;
 
     auto npoints = detector.intersecting_points_h.size() ;
-    GRACE_VERBOSE("We have {} points", npoints) ; 
+    GRACE_VERBOSE("We have {} points", npoints) ;
 
     // initialize local flux array
-    std::array<double,n_fluxes> flux_loc = {0.,0.,0.} ; 
+    std::array<double,n_fluxes> flux_loc = {0.,0.,0.} ;
 
-    // if no local points return 
-    if (npoints == 0 ) return flux_loc ; 
+    // if no local points return
+    if (npoints == 0 ) return flux_loc ;
 
 
-    // copy to host 
+    // copy to host
     auto ivals = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), ivals_d);
     auto ivals_aux = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), ivals_aux_d);
 
 
-    // fetch coord system 
+    // fetch coord system
     auto const& coord_system = grace::coordinate_system::get() ;
 
-    // local reduction 
+    // local reduction
     for(int i=0; i<npoints; ++i) {
 
-        auto ip = detector.intersecting_points_h[i] ; 
+        auto ip = detector.intersecting_points_h[i] ;
         #if GRACE_METRIC_EVOL != GRACE_METRIC_EVOL_Z4
         double gxx{ivals(i,loc_var_idx_t::GXXL)}
                 , gxy{ivals(i,loc_var_idx_t::GXYL)}
@@ -105,7 +105,7 @@ outflows::compute_local_fluxes(
                 , zx{ivals_aux(i,loc_aux_idx_t::ZXL)}
                 , zy{ivals_aux(i,loc_aux_idx_t::ZYL)}
                 , zz{ivals_aux(i,loc_aux_idx_t::ZZL)} ;
-        #else 
+        #else
         double gxx{ivals(i,loc_var_idx_t::GTXXL)}
                 , gxy{ivals(i,loc_var_idx_t::GTXYL)}
                 , gxz{ivals(i,loc_var_idx_t::GTXZL)}
@@ -123,56 +123,56 @@ outflows::compute_local_fluxes(
                 , zx{ivals_aux(i,loc_aux_idx_t::ZXL)}
                 , zy{ivals_aux(i,loc_aux_idx_t::ZYL)}
                 , zz{ivals_aux(i,loc_aux_idx_t::ZZL)} ;
-        #endif 
+        #endif
         #if GRACE_METRIC_EVOL != GRACE_METRIC_EVOL_Z4
         metric_array_t metric{
             {gxx,gxy,gxz,gyy,gyz,gzz}, {betax,betay,betaz}, alp
         } ;
-        #else 
+        #else
         metric_array_t metric{
             {gxx,gxy,gxz,gyy,gyz,gzz}, chi, {betax,betay,betaz}, alp
         } ;
-        #endif 
-        auto r = detector.radius ; 
-        auto theta = detector.angles_h[ip][0] ; 
-        auto phi   = detector.angles_h[ip][1] ; 
-        auto x = detector.points_h[ip].second[0] ; 
-        auto y = detector.points_h[ip].second[1] ; 
-        auto z = detector.points_h[ip].second[2] ; 
+        #endif
+        auto r = detector.radius ;
+        auto theta = detector.angles_h[ip][0] ;
+        auto phi   = detector.angles_h[ip][1] ;
+        auto x = detector.points_h[ip].second[0] ;
+        auto y = detector.points_h[ip].second[1] ;
+        auto z = detector.points_h[ip].second[2] ;
 
         double const one_over_alp = 1./metric.alp() ;
 
         // W^2 = 1 + z^2
-        double const W = Kokkos::sqrt(1+metric.square_vec({zx,zy,zz})) ; 
-        // \tilde{v}^i =  \alpha v^i - beta^i 
-        // but later we need W vtilde so we omit the W at 
-        // the denominator 
-        double const vtx = (alp) * zx - W * metric.beta(0); 
-        double const vty = (alp) * zy - W * metric.beta(1); 
-        double const vtz = (alp) * zz - W * metric.beta(2); 
+        double const W = Kokkos::sqrt(1+metric.square_vec({zx,zy,zz})) ;
+        // \tilde{v}^i =  \alpha v^i - beta^i
+        // but later we need W vtilde so we omit the W at
+        // the denominator
+        double const vtx = (alp) * zx - W * metric.beta(0);
+        double const vty = (alp) * zy - W * metric.beta(1);
+        double const vtz = (alp) * zz - W * metric.beta(2);
 
-        // vt^r 
-        double vtr = vtx * x/r + vty * y/r + vtz * z/r ; 
+        // vt^r
+        double vtr = vtx * x/r + vty * y/r + vtz * z/r ;
 
         // mass flux = sqrtg D \tilde{v}^r
-        double const mass_flux = metric.sqrtg() * rho * vtr ; 
+        double const mass_flux = metric.sqrtg() * rho * vtr ;
 
         // u_t = W ( - alpha + beta_i v^i )
-        auto const W_beta_v = metric.contract_vec_vec(metric._beta, {zx,zy,zz}) ; 
-        auto const u_t = W_beta_v - W * alp ; 
+        auto const W_beta_v = metric.contract_vec_vec(metric._beta, {zx,zy,zz}) ;
+        auto const u_t = W_beta_v - W * alp ;
 
-        // h = 1 + eps + P/rho 
-        double const h = 1 + eps + press/rho ; 
+        // h = 1 + eps + P/rho
+        double const h = 1 + eps + press/rho ;
 
         // angular integral element
-        double const domega = detector.weights_h[ip] ; 
+        double const domega = detector.weights_h[ip] ;
 
         if ( u_t < -1.0 ) {
-            flux_loc[diag_var_idx_t::GEO_UNBOUND] += r * r * domega * mass_flux ; 
+            flux_loc[diag_var_idx_t::GEO_UNBOUND] += r * r * domega * mass_flux ;
         }
 
         if ( h*u_t < -1.0 ) {
-            flux_loc[diag_var_idx_t::BERN_UNBOUND] += r * r * domega * mass_flux ; 
+            flux_loc[diag_var_idx_t::BERN_UNBOUND] += r * r * domega * mass_flux ;
         }
 
         flux_loc[diag_var_idx_t::TOT] += r * r * domega * mass_flux ;
@@ -237,12 +237,12 @@ m1_outflows::compute_local_fluxes(
             double const Fx = ivals(i, loc_var_idx_t::FX1L);
             double const Fy = ivals(i, loc_var_idx_t::FY1L);
             double const Fz = ivals(i, loc_var_idx_t::FZ1L);
-            
+
             // Physical coordinate flux: alpha * F^i - beta^i * E
             double const phys_Fx = alp * Fx - betax * E;
             double const phys_Fy = alp * Fy - betay * E;
             double const phys_Fz = alp * Fz - betaz * E;
-            
+
             // Project onto outward radial normal
             flux_loc[0] += r * r * domega * (phys_Fx * nx + phys_Fy * ny + phys_Fz * nz);
         }
@@ -254,12 +254,12 @@ m1_outflows::compute_local_fluxes(
             double const Fx = ivals(i, loc_var_idx_t::FX2L);
             double const Fy = ivals(i, loc_var_idx_t::FY2L);
             double const Fz = ivals(i, loc_var_idx_t::FZ2L);
-            
+
             // Physical coordinate flux: alpha * F^i - beta^i * E
             double const phys_Fx = alp * Fx - betax * E;
             double const phys_Fy = alp * Fy - betay * E;
             double const phys_Fz = alp * Fz - betaz * E;
-            
+
             // Project onto outward radial normal
             flux_loc[1] += r * r * domega * (phys_Fx * nx + phys_Fy * ny + phys_Fz * nz);
         }
@@ -269,12 +269,12 @@ m1_outflows::compute_local_fluxes(
             double const Fx = ivals(i, loc_var_idx_t::FX3L);
             double const Fy = ivals(i, loc_var_idx_t::FY3L);
             double const Fz = ivals(i, loc_var_idx_t::FZ3L);
-            
+
             // Physical coordinate flux: alpha * F^i - beta^i * E
             double const phys_Fx = alp * Fx - betax * E;
             double const phys_Fy = alp * Fy - betay * E;
             double const phys_Fz = alp * Fz - betaz * E;
-            
+
             // Project onto outward radial normal
             flux_loc[2] += r * r * domega * (phys_Fx * nx + phys_Fy * ny + phys_Fz * nz);
         }
@@ -287,12 +287,12 @@ m1_outflows::compute_local_fluxes(
             double const Fx = ivals(i, loc_var_idx_t::FX4L);
             double const Fy = ivals(i, loc_var_idx_t::FY4L);
             double const Fz = ivals(i, loc_var_idx_t::FZ4L);
-            
+
             // Physical coordinate flux: alpha * F^i - beta^i * E
             double const phys_Fx = alp * Fx - betax * E;
             double const phys_Fy = alp * Fy - betay * E;
             double const phys_Fz = alp * Fz - betaz * E;
-            
+
             // Project onto outward radial normal
             flux_loc[3] += r * r * domega * (phys_Fx * nx + phys_Fy * ny + phys_Fz * nz);
         }
@@ -302,12 +302,12 @@ m1_outflows::compute_local_fluxes(
             double const Fx = ivals(i, loc_var_idx_t::FX5L);
             double const Fy = ivals(i, loc_var_idx_t::FY5L);
             double const Fz = ivals(i, loc_var_idx_t::FZ5L);
-            
+
             // Physical coordinate flux: alpha * F^i - beta^i * E
             double const phys_Fx = alp * Fx - betax * E;
             double const phys_Fy = alp * Fy - betay * E;
             double const phys_Fz = alp * Fz - betaz * E;
-            
+
             // Project onto outward radial normal
             flux_loc[4] += r * r * domega * (phys_Fx * nx + phys_Fy * ny + phys_Fz * nz);
         }
